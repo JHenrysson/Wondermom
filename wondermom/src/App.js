7600314f39fe60f41a048a28591b270d5ef83f4e
Felicia Larsson <felicia.larsson@cmeducations.se>
2020-04-29 11:26:49 +0200

    adding navbar

--- wondermom/src/App.js
+++ wondermom/src/App.js
@@ -1,8 +1,48 @@
-import React from 'react';
 import './App.css';
+import React, { Fragment, useEffect, useRef, useState } from "react"
+import Welcome from "./components/Header/Welcome"
+import Navbar from "./components/Header/Navbar"
 
 function App() {
-  return <> Hello </>;
+  useEffect(() => {
+    return () => {
+      window.removeEventListener("scroll", () => handleScroll)
+    }
+  }, [])
+
+  const [isSticky, setSticky] = useState(false)
+
+  const stickyRef = useRef(null)
+  const handleScroll = () => {
+    window.pageYOffset > stickyRef.current.getBoundingClientRect().bottom
+      ? setSticky(true)
+      : setSticky(false)
+  }
+
+  const debounce = (func, wait = 20, immediate = true) => {
+    let timeOut
+    return () => {
+      let context = this,
+        args = arguments
+      const later = () => {
+        timeOut = null
+        if (!immediate) func.apply(context, args)
+      }
+      const callNow = immediate && !timeOut
+      clearTimeout(timeOut)
+      timeOut = setTimeout(later, wait)
+      if (callNow) func.apply(context, args)
+    }
+  }
+
+  window.addEventListener("scroll", debounce(handleScroll))
+
+  return (
+    <Fragment>
+      <Navbar sticky={isSticky} />
+      <Welcome stickyRef={stickyRef} />
+    </Fragment>
+  )
 }
 
 export default App;
