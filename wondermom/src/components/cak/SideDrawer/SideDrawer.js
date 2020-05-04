41728855bac869e7b6fbcd8fc4f8e47bf691e4c8
Felicia Larsson <felicia.larsson@cmeducations.se>
2020-05-04 11:38:47 +0200

    Navbar update with som dropdown. still working on it do

--- wondermom/src/components/cak/SideDrawer/SideDrawer.js
+++ wondermom/src/components/cak/SideDrawer/SideDrawer.js
@@ -17,13 +17,32 @@ const sideDrawer = props => {
                 <a href="/">Offers</a>
             </li>
             <li>
-                <a href="/">Programs</a>
+                <a href="/">Programs
+                <i className="icon ion-md-arrow-dropdown"></i>
+                </a>
+                <ul className="sub-menu">
+                    <li>
+                        <a href="#">6 week program</a>
+                    </li>
+                    <li>
+                        <a href="#">Program 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Program 3</a>
+                    </li>
+                </ul>
             </li>
             <li>
                 <a href="/">Nutrition</a>
             </li>
             <li>
                 <a href="/">Profile</a>
+            </li>
+            <li>
+                <a href="/">Sign Up</a>
+            </li>
+            <li>
+                <a href="/">Login</a>
             </li>
         </ul>
     </nav>
