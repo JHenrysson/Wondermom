ba04d271fa4232ff0ac41f34ede20b9993c7305f
Felicia Larsson <felicia.larsson@cmeducations.se>
2020-05-06 19:03:27 +0200

    adding btn and 4 grids

--- wondermom/src/pages/Nutrition.js
+++ wondermom/src/pages/Nutrition.js
@@ -5,9 +5,16 @@ import './Nutrition.css';
 
 function Nutrition() {
     return (
+        
+    <>
+    <div id="button">
+        <h2>Nutrition</h2>
+        <button className="btn1">Join now!</button>
+    </div>
+
       <div className="container">
           <div className="row justify-content-center">
-              <div className="col-md-4">
+              <div className="col-md">
                   <div className="card shadow">
                       <div className="card-img-top" alt="Breakfast">
                           <img src="/images/breakfast.jpg"/>
@@ -22,7 +29,7 @@ function Nutrition() {
                 </div>
             </div>
 
-                <div className="col-md-4">
+                <div className="col-md">
                     <div className="card shadow">
                       <div className="card-img-top" alt="lunch">
                       <img src="/images/lunch.jpg"/>
@@ -37,7 +44,7 @@ function Nutrition() {
                 </div>
             </div>
 
-            <div className="col-md-4">
+            <div className="col-md">
                 <div className="card shadow">
                     <div className="card-img-top" alt="dinner">
                     <img src="/images/dinner.jpg"/> 
@@ -51,8 +58,23 @@ function Nutrition() {
                     </div>
                 </div>
             </div>
+            <div className="col-md">
+                    <div className="card shadow">
+                      <div className="card-img-top" alt="lunch">
+                      <img src="/images/lunch.jpg"/>
+                          </div>
+
+                        <div className="card-body text-left">
+                        <h4 className="card-title">Lunch</h4>
+                        <p className="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+    </>
     );
   }
 
