0bc1ac51348bceb9877c5ab443fd83849cee37e1
Felicia Larsson <felicia.larsson@cmeducations.se>
2020-05-13 12:46:46 +0200

    added the breakfast,lunch,dinner cards.

--- wondermom/src/pages/Nutrition.js
+++ wondermom/src/pages/Nutrition.js
@@ -13,11 +13,10 @@ function Nutrition() {
     </div>
 
     <div className="spaceing">
-        <h3>spaceing</h3>
+        <h2>Breakfast</h2>
     </div>
 
       <div className="container">
-          <h2>Breakfast</h2>
           <div className="row justify-content-center">
               <div className="col-md">
                   <div className="card shadow">
@@ -28,7 +27,7 @@ function Nutrition() {
                     <div className="card-body text-left">
                     <h4 className="card-title">Breakfast</h4>
                     <p className="card-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </p>
                     </div>
                 </div>
@@ -43,7 +42,7 @@ function Nutrition() {
                         <div className="card-body text-left">
                         <h4 className="card-title">Lunch</h4>
                         <p className="card-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
                     </div>
                 </div>
@@ -58,7 +57,7 @@ function Nutrition() {
                     <div className="card-body text-left">
                     <h4 className="card-title">Dinner</h4>
                     <p className="card-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </p>
                     </div>
                 </div>
@@ -72,7 +71,141 @@ function Nutrition() {
                         <div className="card-body text-left">
                         <h4 className="card-title">Lunch</h4>
                         <p className="card-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div className="spaceing">
+        <h2>Lunch</h2>
+    </div>
+
+      <div className="container">
+          <div className="row justify-content-center">
+              <div className="col-md">
+                  <div className="card shadow">
+                      <div className="card-img-top">
+                          <img src="/images/breakfast.jpg" alt="Breakfast"/>
+                          </div>
+
+                    <div className="card-body text-left">
+                    <h4 className="card-title">Breakfast</h4>
+                    <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    </div>
+                </div>
+            </div>
+
+                <div className="col-md">
+                    <div className="card shadow">
+                      <div className="card-img-top">
+                      <img src="/images/lunch.jpg" alt="lunch"/>
+                          </div>
+
+                        <div className="card-body text-left">
+                        <h4 className="card-title">Lunch</h4>
+                        <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-md">
+                <div className="card shadow">
+                    <div className="card-img-top">
+                    <img src="/images/dinner.jpg" alt="dinner"/> 
+                        </div>
+
+                    <div className="card-body text-left">
+                    <h4 className="card-title">Dinner</h4>
+                    <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    </div>
+                </div>
+            </div>
+            <div className="col-md">
+                    <div className="card shadow">
+                      <div className="card-img-top">
+                      <img src="/images/lunch.jpg" alt="lunch"/>
+                          </div>
+
+                        <div className="card-body text-left">
+                        <h4 className="card-title">Lunch</h4>
+                        <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div className="spaceing">
+        <h2>Dinner</h2>
+    </div>
+
+      <div className="container">
+          <div className="row justify-content-center">
+              <div className="col-md">
+                  <div className="card shadow">
+                      <div className="card-img-top">
+                          <img src="/images/breakfast.jpg" alt="Breakfast"/>
+                          </div>
+
+                    <div className="card-body text-left">
+                    <h4 className="card-title">Breakfast</h4>
+                    <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    </div>
+                </div>
+            </div>
+
+                <div className="col-md">
+                    <div className="card shadow">
+                      <div className="card-img-top">
+                      <img src="/images/lunch.jpg" alt="lunch"/>
+                          </div>
+
+                        <div className="card-body text-left">
+                        <h4 className="card-title">Lunch</h4>
+                        <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-md">
+                <div className="card shadow">
+                    <div className="card-img-top">
+                    <img src="/images/dinner.jpg" alt="dinner"/> 
+                        </div>
+
+                    <div className="card-body text-left">
+                    <h4 className="card-title">Dinner</h4>
+                    <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    </div>
+                </div>
+            </div>
+            <div className="col-md">
+                    <div className="card shadow">
+                      <div className="card-img-top">
+                      <img src="/images/lunch.jpg" alt="lunch"/>
+                          </div>
+
+                        <div className="card-body text-left">
+                        <h4 className="card-title">Lunch</h4>
+                        <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
                     </div>
                 </div>
