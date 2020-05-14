0ddc660638b092bd58aecc30dc0e5bd44a63a607
carolinearousell <caroline.arousell@cmeducations.se>
2020-05-14 10:46:10 +0200

    put logo in

--- wondermom/src/pages/Home.js
+++ wondermom/src/pages/Home.js
@@ -3,11 +3,29 @@ import './Home.css'
 
 function Home() {
     return (
+    <div>
       <div className="jumbotron">
-        <div className="container">
-          
-        </div>
+      </div>   
+      <div>
+      <img id="logohome"src="/images/logo.png"/>
       </div>
+    <div className="container">
+      <p className="text-lg-left">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.
+        Feugiat nisl pretium fusce id velit ut tortor pretium viverra. 
+        Vulputate odio ut enim blandit volutpat maecenas volutpat blandit. 
+        Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. 
+        Ut morbi tincidunt augue interdum velit euismod in pellentesque massa. 
+        Amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. 
+        Eget sit amet tellus cras adipiscing enim eu turpis egestas. 
+        Purus in massa tempor nec feugiat. 
+        Tristique sollicitudin nibh sit amet commodo nulla facilisi. 
+        Neque convallis a cras semper auctor neque vitae. Convallis
+        posuere morbi leo urna molestie at elementum.
+       </p>
+    </div>
+    </div>
+
     );
   }
 
