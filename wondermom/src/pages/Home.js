a2d5364a647ff02711e5a50379e134f20ca606a4
carolinearousell <caroline.arousell@cmeducations.se>
2020-05-14 11:17:08 +0200

    added text/changed opacity

--- wondermom/src/pages/Home.js
+++ wondermom/src/pages/Home.js
@@ -6,24 +6,30 @@ function Home() {
     <div>
       <div className="jumbotron">
       </div>   
+
       <div>
-      <img id="logohome"src="/images/logo.png" alt="logoimage"/>
+        <img id="logohome"src="/images/logo.png" alt="logoimage"/>
       </div>
+
       <div className="container">
-        <p className="text-lg-left">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.
-          Feugiat nisl pretium fusce id velit ut tortor pretium viverra. 
-          Vulputate odio ut enim blandit volutpat maecenas volutpat blandit. 
-          Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. 
-          Ut morbi tincidunt augue interdum velit euismod in pellentesque massa. 
-          Amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. 
-          Eget sit amet tellus cras adipiscing enim eu turpis egestas. 
-          Purus in massa tempor nec feugiat. 
-          Tristique sollicitudin nibh sit amet commodo nulla facilisi. 
-          Neque convallis a cras semper auctor neque vitae. Convallis
-          posuere morbi leo urna molestie at elementum.
-        </p>
-    
+        <div className="text">
+          <p className="text-lg-left">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.
+            Feugiat nisl pretium fusce id velit ut tortor pretium viverra. 
+            Vulputate odio ut enim blandit volutpat maecenas volutpat blandit. 
+            Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. 
+            Ut morbi tincidunt augue interdum velit euismod in pellentesque massa. 
+            Amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. 
+            Eget sit amet tellus cras adipiscing enim eu turpis egestas. 
+            Purus in massa tempor nec feugiat. 
+            Tristique sollicitudin nibh sit amet commodo nulla facilisi. 
+            Neque convallis a cras semper auctor neque vitae. Convallis
+            posuere morbi leo urna molestie at elementum.
+          </p>
+      </div>
+
+    {/*Carousel will go here once i figure out how to do it*/}  
+
       </div>
 
     </div>
