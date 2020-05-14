86f1d5b1ddef313dda1503d9e4b9789d846e527a
Felicia Larsson <felicia.larsson@cmeducations.se>
2020-05-14 16:36:59 +0200

    added more img

--- wondermom/src/pages/Nutrition.js
+++ wondermom/src/pages/Nutrition.js
@@ -7,7 +7,7 @@ function Nutrition() {
     return (
         
     <>
-    <div id="button">
+    <div id="headline2">
         <h2>Nutrition</h2>
         <button className="btn1">Join now!</button>
     </div>
@@ -21,11 +21,11 @@ function Nutrition() {
               <div className="col-md">
                   <div className="card shadow">
                       <div className="card-img-top">
-                          <img src="/images/breakfast.jpg" alt="Breakfast"/>
+                          <img src="/images/bk1.jpeg" alt="Breakfast"/>
                           </div>
 
                     <div className="card-body text-left">
-                    <h4 className="card-title">Breakfast</h4>
+                    <h4 className="card-title">Title</h4>
                     <p className="card-text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </p>
@@ -36,11 +36,11 @@ function Nutrition() {
                 <div className="col-md">
                     <div className="card shadow">
                       <div className="card-img-top">
-                      <img src="/images/lunch.jpg" alt="lunch"/>
+                      <img src="/images/bk2.jpg" alt="Breakfast2"/>
                           </div>
 
                         <div className="card-body text-left">
-                        <h4 className="card-title">Lunch</h4>
+                        <h4 className="card-title">Title</h4>
                         <p className="card-text">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
@@ -51,11 +51,11 @@ function Nutrition() {
             <div className="col-md">
                 <div className="card shadow">
                     <div className="card-img-top">
-                    <img src="/images/dinner.jpg" alt="dinner"/> 
+                    <img src="/images/bk3.jpg" alt="Breakfast3"/> 
                         </div>
 
                     <div className="card-body text-left">
-                    <h4 className="card-title">Dinner</h4>
+                    <h4 className="card-title">Title</h4>
                     <p className="card-text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </p>
@@ -65,11 +65,11 @@ function Nutrition() {
             <div className="col-md">
                     <div className="card shadow">
                       <div className="card-img-top">
-                      <img src="/images/lunch.jpg" alt="lunch"/>
+                      <img src="/images/bk4.jpeg" alt="Breakfast4"/>
                           </div>
 
                         <div className="card-body text-left">
-                        <h4 className="card-title">Lunch</h4>
+                        <h4 className="card-title">title</h4>
                         <p className="card-text">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
@@ -88,11 +88,11 @@ function Nutrition() {
               <div className="col-md">
                   <div className="card shadow">
                       <div className="card-img-top">
-                          <img src="/images/breakfast.jpg" alt="Breakfast"/>
+                          <img src="/images/lh1.jpg" alt="Breakfast"/>
                           </div>
 
                     <div className="card-body text-left">
-                    <h4 className="card-title">Breakfast</h4>
+                    <h4 className="card-title">Title</h4>
                     <p className="card-text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </p>
@@ -103,11 +103,11 @@ function Nutrition() {
                 <div className="col-md">
                     <div className="card shadow">
                       <div className="card-img-top">
-                      <img src="/images/lunch.jpg" alt="lunch"/>
+                      <img src="/images/lh2.jpg" alt="lunch"/>
                           </div>
 
                         <div className="card-body text-left">
-                        <h4 className="card-title">Lunch</h4>
+                        <h4 className="card-title">Title</h4>
                         <p className="card-text">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
@@ -118,11 +118,11 @@ function Nutrition() {
             <div className="col-md">
                 <div className="card shadow">
                     <div className="card-img-top">
-                    <img src="/images/dinner.jpg" alt="dinner"/> 
+                    <img src="/images/lh3.jpg" alt="dinner"/> 
                         </div>
 
                     <div className="card-body text-left">
-                    <h4 className="card-title">Dinner</h4>
+                    <h4 className="card-title">Title</h4>
                     <p className="card-text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </p>
@@ -132,11 +132,11 @@ function Nutrition() {
             <div className="col-md">
                     <div className="card shadow">
                       <div className="card-img-top">
-                      <img src="/images/lunch.jpg" alt="lunch"/>
+                      <img src="/images/lh4.jpg" alt="lunch"/>
                           </div>
 
                         <div className="card-body text-left">
-                        <h4 className="card-title">Lunch</h4>
+                        <h4 className="card-title">Title</h4>
                         <p className="card-text">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
@@ -155,11 +155,11 @@ function Nutrition() {
               <div className="col-md">
                   <div className="card shadow">
                       <div className="card-img-top">
-                          <img src="/images/breakfast.jpg" alt="Breakfast"/>
+                          <img src="/images/dn1.jpeg" alt="Dinner1"/>
                           </div>
 
                     <div className="card-body text-left">
-                    <h4 className="card-title">Breakfast</h4>
+                    <h4 className="card-title">Title</h4>
                     <p className="card-text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </p>
@@ -170,11 +170,11 @@ function Nutrition() {
                 <div className="col-md">
                     <div className="card shadow">
                       <div className="card-img-top">
-                      <img src="/images/lunch.jpg" alt="lunch"/>
+                      <img src="/images/dn2.jpg" alt="Dinner2"/>
                           </div>
 
                         <div className="card-body text-left">
-                        <h4 className="card-title">Lunch</h4>
+                        <h4 className="card-title">Title</h4>
                         <p className="card-text">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
@@ -185,11 +185,11 @@ function Nutrition() {
             <div className="col-md">
                 <div className="card shadow">
                     <div className="card-img-top">
-                    <img src="/images/dinner.jpg" alt="dinner"/> 
+                    <img src="/images/dn3.jpg" alt="Dinner3"/> 
                         </div>
 
                     <div className="card-body text-left">
-                    <h4 className="card-title">Dinner</h4>
+                    <h4 className="card-title">Title</h4>
                     <p className="card-text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </p>
@@ -199,11 +199,11 @@ function Nutrition() {
             <div className="col-md">
                     <div className="card shadow">
                       <div className="card-img-top">
-                      <img src="/images/lunch.jpg" alt="lunch"/>
+                      <img src="/images/dn4.jpeg" alt="Dinner4"/>
                           </div>
 
                         <div className="card-body text-left">
-                        <h4 className="card-title">Lunch</h4>
+                        <h4 className="card-title">Title</h4>
                         <p className="card-text">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
