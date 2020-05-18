ceaa169215ed830ffd9884429fb68004343fb69f
carolinearousell <caroline.arousell@cmeducations.se>
2020-05-18 14:24:16 +0200

    attempt at making a carousel

---
+++ wondermom/src/components/carousel/BootstrapCarousel.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'  
+export class BootstrapCarousel extends Component {  
+
+    render() {  
+
+    <div>  
+        <div class='container-fluid' >  
+            <div className="row title" style={{ marginBottom: "20px" }} >  
+                <div class="col-sm-12 btn btn-warning">  
+                    How To Use Bootstrap Carousel In ReactJS  
+                </div>  
+            </div>  
+        </div>  
+
+        <div className='container-fluid' > 
+
+            <Carousel>  
+                <Carousel.Item style={{'height':"300px"}} > 
+                    <img style={{'height':"300px"}} 
+                        className="d-block w-100"
+                        src={'assets/img/img2.jpg'}  />  
+
+                            <Carousel.Caption>  
+                                <h3>First Demo </h3>  
+                            </Carousel.Caption>  
+
+                    </Carousel.Item  >  
+
+                    <Carousel.Item style={{'height':"300px"}}>  
+                        <img style={{'height':"300px"}}  
+                        className="d-block w-100"  
+                        src={'assets/img/img1.jpg'}    />  
+
+                            <Carousel.Caption>  
+                                <h3>Second Demo</h3>  
+                            </Carousel.Caption>  
+
+                    </Carousel.Item>  
+
+                    <Carousel.Item style={{'height':"300px"}}>  
+                        <img style={{'height':"300px"}}  
+                        className="d-block w-100"  
+                        src={'assets/img/img3.jpg'}   />  
+
+                            <Carousel.Caption>  
+                                <h3>Third Demo</h3>  
+                            </Carousel.Caption>  
+
+                    </Carousel.Item>  
+
+            </Carousel>  
+        </div>  
+    </div>  
+                  
+        }  
+}  
+
+export default BootstrapCarousel  
+
