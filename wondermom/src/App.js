bf9d77315e7cff06a76fbca3616cf5f41f664949
Felicia Larsson <felicia.larsson@cmeducations.se>
2020-05-19 10:24:59 +0200

    Update App.js

--- wondermom/src/App.js
+++ wondermom/src/App.js
@@ -14,6 +14,7 @@ import About from './pages/About';
 import Home from './pages/Home';
 import Programs from './pages/SixWeekProgram';
 import Offers from './pages/Offers';
+import DayOne from "./pages/DayOne";
 
 // This is the animation of the navbar
 class App extends Component {
