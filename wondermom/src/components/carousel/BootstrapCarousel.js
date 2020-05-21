c8167eb2669753f209bcea7fb008c7f4ff46dc6b
carolinearousell <caroline.arousell@cmeducations.se>
2020-05-21 16:13:40 +0200

    chnaged logo and resized carousel

--- wondermom/src/components/carousel/BootstrapCarousel.js
+++ wondermom/src/components/carousel/BootstrapCarousel.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';  
 import Carousel from 'react-bootstrap/Carousel' 
+import './BootstrapCarousel.css'
 class BootstrapCarousel extends Component {  
 
     render() {  
 
     return (
         
-    <div>  
-        <div class='container-fluid' >  
-            <div className="row title" style={{ marginBottom: "20px" }} >  
-                <div class="col-sm-12 btn btn-warning">  
-                    How To Use Bootstrap Carousel In ReactJS  
-                </div>  
-            </div>  
-        </div>  
-
-        <div className='container-fluid' > 
+    <div>
 
             <Carousel>  
-                <Carousel.Item style={{'height':"300px"}} > 
-                    <img style={{'height':"300px"}} 
-                        className="d-block w-100"
+                <Carousel.Item> 
+                    <img className="d-block w-100"
                         src={'/images/workoutabs.jpg'}  />  
 
                             <Carousel.Caption>  
@@ -29,20 +20,18 @@ class BootstrapCarousel extends Component {
 
                 </Carousel.Item  >  
 
-                <Carousel.Item style={{'height':"300px"}}>  
-                    <img style={{'height':"300px"}}  
-                    className="d-block w-100"  
+                <Carousel.Item>  
+                    <img className="d-block w-100"  
                     src={'/images/workoutbutt.jpg'}    />  
 
                         <Carousel.Caption>  
                             <h3>Second Demo</h3>  
                         </Carousel.Caption>  
 
-                </Carousel.Item>  
+                </Carousel.Item >  
 
-                <Carousel.Item style={{'height':"300px"}}>  
-                    <img style={{'height':"300px"}}  
-                    className="d-block w-100"  
+                <Carousel.Item>  
+                    <img className="d-block w-100"  
                     src={'images/workouthug.jpg'}   />  
 
                         <Carousel.Caption>  
@@ -52,8 +41,7 @@ class BootstrapCarousel extends Component {
                 </Carousel.Item>  
 
             </Carousel>  
-        </div>  
-    </div>  
+        </div>   
             
     );
     }  
