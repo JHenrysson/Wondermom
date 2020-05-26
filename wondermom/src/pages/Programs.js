3ed4daa87770dfae3cce86e1ba8a7e3bd1c665e4
Elinor Kero <elinor.kero@cmeducations.se>
2020-05-26 11:36:56 +0200

    Added margins and changed from class to className

--- wondermom/src/pages/Programs.js
+++ wondermom/src/pages/Programs.js
@@ -3,31 +3,31 @@ import './Programs.css';
 
 function Programs() {
     return (
-        <div id="main">
+        <div className="main">
             <h1>Programs</h1>
             <div id="main">
-                <div class="card-deck">
-                    <div class="card">
+                <div className="card-deck">
+                    <div className="card">
                         <img src="./images/6weeks.jpg" class="card-img-top" alt="6weeks"></img>
-                        <div class="card-body">
-                            <h5 class="card-title">6 week program</h5>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div className="card-body">
+                            <h5 className="card-title">6 week program</h5>
+                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                             <button className="card-link">Try this program</button>
                         </div>
                     </div>
-                    <div class="card">
+                    <div className="card">
                         <img src="./images/8weeks.jpg" class="card-img-top" alt="8weeks"></img>
-                        <div class="card-body">
-                            <h5 class="card-title">8 week program</h5>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div className="card-body">
+                            <h5 className="card-title">8 week program</h5>
+                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                             <button className="card-link">Try this program</button>
                         </div>
                     </div>
-                    <div class="card">
+                    <div className="card">
                         <img src="./images/12weeks.jpg" class="card-img-top" alt="12weeks"></img>
-                        <div class="card-body">
-                            <h5 class="card-title">12 week program</h5>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div className="card-body">
+                            <h5 className="card-title">12 week program</h5>
+                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                             <button className="card-link">Try this program</button>
                         </div>
                     </div>
