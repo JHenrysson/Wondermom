bc662b1299dd3bf44dda06ac763ced9ed8a7a669
carolinearousell <caroline.arousell@cmeducations.se>
2020-05-27 17:20:17 +0200

    tried fixing carousel and text locations

--- wondermom/src/pages/Home.js
+++ wondermom/src/pages/Home.js
@@ -14,29 +14,28 @@ function Home() {
       </div>
 
       <div className="container">
-        <div className="text">
-          <p className="text-lg-left">
+        <div className="">
+          <p className="text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.
             Feugiat nisl pretium fusce id velit ut tortor pretium viverra. 
             Vulputate odio ut enim blandit volutpat maecenas volutpat blandit. 
             Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. 
             Ut morbi tincidunt augue interdum velit euismod in pellentesque massa. 
             Amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. 
-            Eget sit amet tellus cras adipiscing enim eu turpis egestas. 
-            Purus in massa tempor nec feugiat. 
-            Tristique sollicitudin nibh sit amet commodo nulla facilisi. 
-            Neque convallis a cras semper auctor neque vitae. Convallis
-            posuere morbi leo urna molestie at elementum.
-            Ut morbi tincidunt augue interdum velit euismod in pellentesque massa. 
-            Amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. 
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.
+            Feugiat nisl pretium fusce id velit ut tortor pretium viverra. 
+            Vulputate odio ut enim blandit volutpat maecenas volutpat blandit. 
           </p>
-      </div>
 
-      <BootstrapCarousel></BootstrapCarousel>  
+        </div>
 
-      </div>
+        <div className="carouselsize">
+          <BootstrapCarousel></BootstrapCarousel>  
+        </div>
 
     </div>
+
+  </div>
    
 
     );
