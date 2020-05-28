8933d7d8454a3698c63941f2161c3daaf6898efc
JHenrysson <56089745+JHenrysson@users.noreply.github.com>
2020-05-28 16:40:11 +0200

    footer
    added icon links to social media
    links to the different sites on the page
    button to sign up

--- wondermom/src/pages/Footer.js
+++ wondermom/src/pages/Footer.js
@@ -2,47 +2,80 @@ import React from 'react';
 import './Footer.css';
 
 const footer = props => (
-    <footer className="page-footer font-small stylish-color-dark pt-4 ">
-        <hr></hr>
-        <div className="container text-center text-md-left">
-        </div>
 
+    <footer>
         <hr/>
-
+        <ul className="list-unstyled list-inline text-center py-2">
+            <div className="row">
+                <li className="col-sm list-inline-item">
+                    <h5>
+                        <a href="/home">
+                            Home
+                        </a>
+                    </h5>
+                </li>
+                <li className="col-sm list-inline-item">
+                    <h5>
+                        <a href="/about">
+                            About
+                        </a>
+                    </h5>
+                </li>
+                <li className="col-sm list-inline-item">
+                    <h5>
+                        <a href="/programs">
+                            Program
+                        </a>
+                    </h5>
+                </li>
+                <li className="col-sm list-inline-item">
+                    <h5>
+                        <a href="/nutrition">
+                            Nutrition
+                        </a>
+                    </h5>
+                </li>
+            </div>
+        </ul>
+        <hr/>
+        <div class="text">
+            <p className="text-center">Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet
+                fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex
+                communi. At nos hinc posthac, sitientis piros Afros. </p>
+        </div>
+        <hr/>
         <ul className="list-unstyled list-inline text-center py-2">
             <li className="list-inline-item">
-                <h5 className="mb-1">Monthly Newsletter </h5>
+                <h5 className="mb-1">Subscribe </h5>
             </li>
             <li className="list-inline-item">
-                <a href="/" className="btn btn-outline-light ">Sign up!</a>
+                <button type="button" class="btn btn-outline-light">Sign Up</button>
             </li>
         </ul>
-
         <hr/>
-
         <ul className="list-unstyled list-inline text-center">
             <li className="list-inline-item">
-                <a href="/" className="btn-floating btn-fb mx-1">
-                    <i className="fab fa-facebook-f"> </i>
-                </a>
+                <a href="https://www.facebook.com/" className="btn-floating btn-fb mx-1">
+                    <i className="fab fa-facebook-f"> </i></a>
             </li>
             <li className="list-inline-item">
-                <a href="/" className="btn-floating btn-tw mx-1">
+                <a href="https://twitter.com/" className="btn-floating btn-tw mx-1">
                     <i className="fab fa-twitter"> </i>
                 </a>
             </li>
             <li className="list-inline-item">
-                <a href="/" className="btn-floating btn-gplus mx-1">
-                    <i className="fab fa-google-plus-g"> </i>
+                <a href="https://www.instagram.com/" className="btn-floating btn-gplus mx-1">
+                    <i className="fab fa-instagram"> </i>
                 </a>
             </li>
             <li className="list-inline-item">
-                <a href="/" className="btn-floating btn-li mx-1">
+                <a href="https://www.linkedin.com/" className="btn-floating btn-li mx-1">
                     <i className="fab fa-linkedin-in"> </i>
                 </a>
             </li>
         </ul>
     </footer>
+
 );
 
 export default footer;
