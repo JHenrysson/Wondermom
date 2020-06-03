bd8f6358e3f4bd9efcf1f6fc9817d016305dc61a
Felicia Larsson <felicia.larsson@cmeducations.se>
2020-06-03 11:47:12 +0200

    making my cards responsive

--- wondermom/src/pages/Nutritionpages/Dinner.js
+++ wondermom/src/pages/Nutritionpages/Dinner.js
@@ -4,138 +4,128 @@ import './Nutrition.css';
 function Dinner() {
     return (
         <>
+    <>
         <div id="headline2">
             <h1>Dinner</h1>
         </div>
     
         <div className="spaceing">
         </div>
-    
-      <div className="container">
-      <div className="card-deck">
-          <div className="row justify-content-center">
-              <div className="col-12 col-md-6 col-lg-3">
-                  <div className="card shadow">
-                      <div className="card-img-top">
-                          <img src="/images/dn1.jpeg" alt="Dinner1"/>
-                          </div>
-
-                    <div className="card-body text-center">
-                    <h4 className="card-title text-center">Title</h4>
-                    <p className="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                    
-                    </div>
-                </div>
-            </div>
-
-                <div className="col-12 col-md-6 col-lg-3">
-                    <div className="card shadow">
-                      <div className="card-img-top">
-                      <img src="/images/dn2.jpg" alt="Dinner2"/>
-                          </div>
-
-                        <div className="card-body text-center">
-                        <h4 className="card-title text-center">Title</h4>
-                        <p className="card-text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </p>
-                        
-                    </div>
-                </div>
-            </div>
-
-            <div className="col-12 col-md-6 col-lg-3">
-                <div className="card shadow">
-                    <div className="card-img-top">
-                    <img src="/images/dn2.jpg" alt="Dinner3"/> 
-                        </div>
-
-                    <div className="card-body text-center">
-                    <h4 className="card-title text-center">Title</h4>
-                    <p className="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                    
-                    </div>
-                </div>
-            </div>
-            <div className="col-12 col-md-6 col-lg-3">
-                    <div className="card shadow">
-                      <div className="card-img-top">
-                      <img src="/images/dn1.jpeg" alt="Dinner4"/>
-                          </div>
-
-                        <div className="card-body text-center">
-                        <h4 className="card-title text-center">Title</h4>
-                        <p className="card-text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </p>
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    </div>
-    <div className="spaceing">
-        </div>
-    
-      <div className="container">
-      <div className="card-deck">
-          <div className="row justify-content-center">
-              <div className="col-12 col-md-6 col-lg-3">
-                  <div className="card shadow">
-                      <div className="card-img-top">
-                          <img src="/images/dn1.jpeg" alt="Dinner1"/>
-                          </div>
-
-                    <div className="card-body text-center">
-                    <h4 className="card-title text-center">Title</h4>
-                    <p className="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                    
-                    </div>
-                </div>
-            </div>
-
-                <div className="col-12 col-md-6 col-lg-3">
-                    <div className="card shadow">
-                      <div className="card-img-top">
-                      <img src="/images/dn2.jpg" alt="Dinner2"/>
-                          </div>
-
-                        <div className="card-body text-center">
-                        <h4 className="card-title text-center">Title</h4>
-                        <p className="card-text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </p>
-                        
-                    </div>
-                </div>
-            </div>
-
-            <div className="col-12 col-md-6 col-lg-3">
-                <div className="card shadow">
-                    <div className="card-img-top">
-                    <img src="/images/dn2.jpg" alt="Dinner3"/> 
-                        </div>
-
-                    <div className="card-body text-center">
-                    <h4 className="card-title text-center">Title</h4>
-                    <p className="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
         
+        <div className="container">
+            <div className="card-columns">   
+          
+                <div className="card shadow">
+                        <div className="card-img-top">
+                            <img src="./images/dn1.jpeg" alt="Breakfast"/>
+                            </div>
+
+                        <div className="card-body text-center">
+                        <h4 className="card-title text-center">Title</h4>
+                        <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        </div>
+                      
+                </div>
+
+                
+                <div className="card shadow">
+                      <div className="card-img-top">
+                      <img src="/images/dn2.jpg" alt="lunch"/>
+                          </div>
+
+                        <div className="card-body text-center">
+                        <h4 className="card-title text-center">Title</h4>
+                        <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        </div>
+                       
+                
+                </div>
+
+                <div className="card shadow">
+                    <div className="card-img-top">
+                    <img src="/images/dn1.jpeg" alt="dinner"/> 
+                        </div>
+
+                    <div className="card-body text-center">
+                    <h4 className="card-title text-center">Title</h4>
+                    <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    </div>
+                   
+                </div>
+
+                <div className="card shadow">
+                      <div className="card-img-top">
+                      <img src="/images/dn2.jpg" alt="lunch"/>
+                          </div>
+
+                        <div className="card-body text-center">
+                        <h4 className="card-title text-center">Title</h4>
+                        <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
                     </div>
                 </div>
             </div>
-            <div className="col-12 col-md-6 col-lg-3">
-                    <div className="card shadow">
+        </div>
+        <div className="spaceing">
+        </div>
+        
+        <div className="container">
+            <div className="card-columns">   
+          
+                <div className="card shadow">
+                        <div className="card-img-top">
+                            <img src="./images/dn1.jpeg" alt="Breakfast"/>
+                            </div>
+
+                        <div className="card-body text-center">
+                        <h4 className="card-title text-center">Title</h4>
+                        <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        
+                        </div>
+                </div>
+
+                
+                <div className="card shadow">
                       <div className="card-img-top">
-                      <img src="/images/dn2.jpg" alt="Dinner4"/>
+                      <img src="/images/dn2.jpg" alt="lunch"/>
+                          </div>
+
+                        <div className="card-body text-center">
+                        <h4 className="card-title text-center">Title</h4>
+                        <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        
+                    </div>
+                
+                </div>
+
+                <div className="card shadow">
+                    <div className="card-img-top">
+                    <img src="/images/dn1.jpeg" alt="dinner"/> 
+                        </div>
+
+                    <div className="card-body text-center">
+                    <h4 className="card-title text-center">Title</h4>
+                    <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    
+                    </div>
+                </div>
+
+                <div className="card shadow">
+                      <div className="card-img-top">
+                      <img src="/images/dn2.jpg" alt="lunch"/>
                           </div>
 
                         <div className="card-body text-center">
@@ -148,8 +138,7 @@ function Dinner() {
                 </div>
             </div>
         </div>
-    </div>
-    </div>
+    </>
         </>
         );
       }
--- wondermom/src/pages/Nutritionpages/Snacks.js
+++ wondermom/src/pages/Nutritionpages/Snacks.js
@@ -3,117 +3,66 @@ import './Nutrition.css';
 
 function Snacks() {
     return (
-        <div>
-            <div id="headline2">
-                <h1>Snacks</h1>
-            </div>
-        
-            <div className="spaceing"></div>
-        
-            <div className="container">
-            <div className="card-deck">
-                <div className="row jusify-content-center">
-                    <div className="col-12 col-md-6 col-lg-3">
-                        <div className="card shadow">
-                            <div className="card-img-top">
-                                <img src="/images/bk1.jpeg" alt="Breakfast"/>
-                            </div>
-    
-                            <div className="card-body text-center">
-                                <h4 className="card-title">Title</h4>
-                                <p className="card-text">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-    
-                    <div className="col-12 col-md-6 col-lg-3">
-                        <div className="card shadow">
-                            <div className="card-img-top">
-                                <img src="/images/bk2.jpg" alt="Breakfast2"/>
-                            </div>
-    
-                            <div className="card-body text-center">
-                                <h4 className="card-title text-center">Title</h4>
-                                <p className="card-text">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-    
-                    <div className="col-12 col-md-6 col-lg-3">
-                        <div className="card shadow">
-                            <div className="card-img-top">
-                                <img src="/images/bk3.jpg" alt="Breakfast3"/> 
-                            </div>
-        
-                            <div className="card-body text-center">
-                                <h4 className="card-title text-center">Title</h4>
-                                <p className="card-text">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-12 col-md-6 col-lg-3">
-                        <div className="card shadow">
-                            <div className="card-img-top">
-                            <img src="/images/bk4.jpeg" alt="Breakfast4"/>
-                            </div>
-                                <div className="card-body text-center">
-                                <h4 className="card-title text-center">Title</h4>
-                                <p className="card-text">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <>
+        <div id="headline2">
+            <h1>Snacks</h1>
         </div>
-
-        <div className="spaceing"></div>
     
+        <div className="spaceing">
+        </div>
+        
         <div className="container">
-            <div className="card-deck">
-              <div className="row jusify-content-center">
-                  <div className="col-12 col-md-6 col-lg-3">
-                      <div className="card shadow">
-                          <div className="card-img-top">
-                            <img src="/images/bk1.jpeg" alt="Breakfast"/>
+            <div className="card-columns">   
+          
+                <div className="card shadow">
+                        <div className="card-img-top">
+                            <img src="./images/bk1.jpeg" alt="Breakfast"/>
                             </div>
+
                         <div className="card-body text-center">
-                        <h4 className="card-title">Title</h4>
+                        <h4 className="card-title text-center">Title</h4>
                         <p className="card-text">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
-                    </div>
+                        </div>
+                      
                 </div>
-            </div>
-    
-            <div className="col-12 col-md-6 col-lg-3">
+
+                
+                <div className="card shadow">
+                      <div className="card-img-top">
+                      <img src="/images/bk2.jpg" alt="lunch"/>
+                          </div>
+
+                        <div className="card-body text-center">
+                        <h4 className="card-title text-center">Title</h4>
+                        <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        </div>
+                       
+                
+                </div>
+
                 <div className="card shadow">
                     <div className="card-img-top">
-                    <img src="/images/bk2.jpg" alt="Breakfast2"/>
+                    <img src="/images/bk3.jpg" alt="dinner"/> 
+                        </div>
+
+                    <div className="card-body text-center">
+                    <h4 className="card-title text-center">Title</h4>
+                    <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
                     </div>
-    
-                        <div className="card-body text-center">
-                        <h4 className="card-title text-center">Title</h4>
-                        <p className="card-text">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </p>
-                    </div>
+                   
                 </div>
-            </div>
-    
-                <div className="col-12 col-md-6 col-lg-3">
-                    <div className="card shadow">
-                        <div className="card-img-top">
-                        <img src="/images/bk3.jpg" alt="Breakfast3"/> 
-                            </div>
-    
+
+                <div className="card shadow">
+                      <div className="card-img-top">
+                      <img src="/images/bk4.jpeg" alt="lunch"/>
+                          </div>
+
                         <div className="card-body text-center">
                         <h4 className="card-title text-center">Title</h4>
                         <p className="card-text">
@@ -122,25 +71,72 @@ function Snacks() {
                     </div>
                 </div>
             </div>
+        </div>
+        <div className="spaceing">
+        </div>
+        
+        <div className="container">
+            <div className="card-columns">   
+          
+                <div className="card shadow">
+                        <div className="card-img-top">
+                            <img src="./images/bk1.jpeg" alt="Breakfast"/>
+                            </div>
 
-                <div className="col-12 col-md-6 col-lg-3">
-                        <div className="card shadow">
-                          <div className="card-img-top">
-                          <img src="/images/bk4.jpeg" alt="Breakfast4"/>
-                              </div>
-    
-                            <div className="card-body text-center">
-                            <h4 className="card-title text-center">Title</h4>
-                            <p className="card-text">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            </p>
+                        <div className="card-body text-center">
+                        <h4 className="card-title text-center">Title</h4>
+                        <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        
                         </div>
+                </div>
+
+                
+                <div className="card shadow">
+                      <div className="card-img-top">
+                      <img src="/images/bk2.jpg" alt="lunch"/>
+                          </div>
+
+                        <div className="card-body text-center">
+                        <h4 className="card-title text-center">Title</h4>
+                        <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        
+                    </div>
+                
+                </div>
+
+                <div className="card shadow">
+                    <div className="card-img-top">
+                    <img src="/images/bk3.jpg" alt="dinner"/> 
+                        </div>
+
+                    <div className="card-body text-center">
+                    <h4 className="card-title text-center">Title</h4>
+                    <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    
+                    </div>
+                </div>
+
+                <div className="card shadow">
+                      <div className="card-img-top">
+                      <img src="/images/bk4.jpeg" alt="lunch"/>
+                          </div>
+
+                        <div className="card-body text-center">
+                        <h4 className="card-title text-center">Title</h4>
+                        <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </>
 );
 }
 
