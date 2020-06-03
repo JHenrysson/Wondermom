9d37d65829cb0f6e8f9064559064d5cdc7bd709c
Mira Koivula <mira.koivula.1987@gmail.com>
2020-06-03 22:57:04 +0200

    Update About.js
    Have added som stuff to about page.

--- wondermom/src/pages/About.js
+++ wondermom/src/pages/About.js
@@ -1,11 +1,110 @@
 import React from 'react';
+import './About.css';
+
 
 function About() {
     return (
-      <div>
-        <h1>About</h1>
-      </div>
-    );
-  }
+      <>
+
+
+<div class="container-fluid p-2 p-md-5 m-md-3 text-center">
+  <div class="col-md-5 p-lg-5 mx-auto my-5">
+    <h1 class="display-4">About us</h1>
+    <p class="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple’s marketing pages.</p>
+    </div>
+    </div>
+    
+
+<br></br>
+<br></br>
+<br></br>
+
+    <section class="container-fluid px-0">
+        <div class="row align-items-center content">
+            <div class="col-md-6">
+            <img src="./images/about.jpg" alt="" class="img-fluid"></img>
+            </div>
+            <div class="col-md-6 text-center">
+                <div class="row justify-content-center">
+                    <div class="col-10 col-lg-8 blurb mb-5 mb-md-0">
+                        <h2>Our mission</h2>
+                        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, iste molestiae
+                            beatae, maiores deserunt
+                            in voluptatibus
+                            aspernatur architecto excepturi delectus soluta? Ipsa, deleniti dolorem hic consequatur
+                            repellat eveniet quidem
+                            voluptate necessitatibus dolorum delectus minus vitae, ut, veritatis sint ipsum magnam
+                            autem nam ex deserunt debitis
+                            eaque ratione! Nobis, quidem assumenda.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
+        <br></br>
+        <br></br>
+
+        <section class="container-fluid px-0">
+        <div class="row align-items-center content">
+            <div class="col-md-6">
+            <img src="./images/about.jpg" alt="" class="img-fluid"></img>
+            </div>
+            <div class="col-md-6 text-center">
+                <div class="row justify-content-center">
+                    <div class="col-10 col-lg-8 blurb mb-5 mb-md-0">
+                        <h2>Our mission</h2>
+                        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, iste molestiae
+                            beatae, maiores deserunt
+                            in voluptatibus
+                            aspernatur architecto excepturi delectus soluta? Ipsa, deleniti dolorem hic consequatur
+                            repellat eveniet quidem
+                            voluptate necessitatibus dolorum delectus minus vitae, ut, veritatis sint ipsum magnam
+                            autem nam ex deserunt debitis
+                            eaque ratione! Nobis, quidem assumenda.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
+<br></br>
+<br></br>
+<br></br>
+        <section class="container-fluid px-0">
+        <div class="row align-items-center content">
+            <div class="col-md-6">
+            <img src="./images/about.jpg" alt="" class="img-fluid"></img>
+            </div>
+            <div class="col-md-6 text-center">
+                <div class="row justify-content-center">
+                    <div class="col-10 col-lg-8 blurb mb-5 mb-md-0">
+                        <h2>Our mission</h2>
+                        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, iste molestiae
+                            beatae, maiores deserunt
+                            in voluptatibus
+                            aspernatur architecto excepturi delectus soluta? Ipsa, deleniti dolorem hic consequatur
+                            repellat eveniet quidem
+                            voluptate necessitatibus dolorum delectus minus vitae, ut, veritatis sint ipsum magnam
+                            autem nam ex deserunt debitis
+                            eaque ratione! Nobis, quidem assumenda.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
+
+
+        
+  
+   
+
+  
+     </>
+  
+
+      
+      );
+    }
+
+
 
   export default About;
