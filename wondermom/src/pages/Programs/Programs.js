b2013860b8fe37a9f60d5dc28bbefc7b3a6bd34d
Elinor Kero <elinor.kero@cmeducations.se>
2020-06-04 11:29:53 +0200

    added banner image and jumbotron for programs page
    however the image won't show up in the jumbotron

--- wondermom/src/pages/Programs/Programs.js
+++ wondermom/src/pages/Programs/Programs.js
@@ -1,43 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Programs.css';
 
-function Programs() {
-    return (
-        <div className="main">
-            <h1>Programs</h1>
-            <div id="main">
-                <div className="card-deck">
-                    <div className="card">
-                        <img src="./images/6weeks.jpg" className="card-img-top" alt="6weeks"></img>
-                        <div className="card-body">
-                            <h4 className="card-title">6 week program</h4>
-                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+class Programs extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="main">
+
+                <div id="jumbotronForPrograms" className="jumbotron jumbotron-fluid">
+                    <div className="container"></div>
+                </div>
+
+                <h1>Programs</h1>
+                <div id="main">
+                    <div className="card-deck">
+                        <div className="card">
+                            <img src="./images/6weeks.jpg" className="card-img-top" alt="6weeks"></img>
+                            <div className="card-body">
+                                <h4 className="card-title">6 week program</h4>
+                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                                 eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                            <a href="/SixWeekProgram" className="card-link justify-content-center">Try this program</a>
+                                <a href="/SixWeekProgram" className="card-link justify-content-center">Try this program</a>
+                            </div>
                         </div>
-                    </div>
-                    <div className="card">
-                        <img src="./images/8weeks.jpg" className="card-img-top" alt="8weeks"></img>
-                        <div className="card-body">
-                            <h4 className="card-title">8 week program</h4>
-                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        <div className="card">
+                            <img src="./images/8weeks.jpg" className="card-img-top" alt="8weeks"></img>
+                            <div className="card-body">
+                                <h4 className="card-title">8 week program</h4>
+                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                                 eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                            <button className="card-link">Try this program</button>
+                                <button className="card-link">Try this program</button>
+                            </div>
                         </div>
-                    </div>
-                    <div className="card">
-                        <img src="./images/12weeks.jpg" className="card-img-top" alt="12weeks"></img>
-                        <div className="card-body">
-                            <h4 className="card-title">12 week program</h4>
-                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        <div className="card">
+                            <img src="./images/12weeks.jpg" className="card-img-top" alt="12weeks"></img>
+                            <div className="card-body">
+                                <h4 className="card-title">12 week program</h4>
+                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                                 eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                            <button className="card-link">Try this program</button>
+                                <button className="card-link">Try this program</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Programs;
