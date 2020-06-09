098669d4fa89ac80326e3336659a1acc2e263f9d
Mira Koivula <mira.koivula.1987@gmail.com>
2020-06-09 22:00:14 +0200

    Update About.js
    changed the class to className

--- wondermom/src/pages/About.js
+++ wondermom/src/pages/About.js
@@ -7,22 +7,22 @@ function About() {
       <>
 
 <div className="jumbotron jumbotron-fluid d-flex justify-content-center align-items-center" id="aboutjumbotron">
-  <div class="container text-center">
-  <h1 class="display-2">About us</h1>
-  <p class="lead">We’re here to keep mamas around the world inspired and motivated to live healthier every day. We’ll be your best friend in your back pocket, coaching you on how to stay fit, eat well, and live more mindfully.</p>
+  <div className="container text-center">
+  <h1 className="display-2">About us</h1>
+  <p className="lead">We want to inspire and motivate to live healthier every day. We’ll be your best friend in your back pocket, coaching you on how to stay fit, eat well, and live more mindfully.</p>
 </div>
 </div>
 
-      <section class="container-fluid px-0">
-        <div class="row align-items-center content">
-          <div class="col-md-6 order-2 order-md-1">
+      <section className="container-fluid px-0">
+        <div className="row align-items-center content">
+          <div className="col-md-6 order-2 order-md-1">
             <img src="./images/heart.jpg" alt="" class="img-fluid"></img>
             </div>    
-            <div class="col-md-6 text-center order-1 order-md-2">
-                <div class="row justify-content-center">
-                    <div class="col-10 col-lg-8 mb-5 mb-md-0 blurb m-4 m-md-0">
+            <div className="col-md-6 text-center order-1 order-md-2">
+                <div classNames="row justify-content-center">
+                    <div className="col-10 col-lg-8 mb-5 mb-md-0 blurb m-4 m-md-0">
                         <h3>Our mission</h3>
-                        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, iste molestiae
+                        <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, iste molestiae
                             beatae, maiores deserunt
                             in voluptatibus
                             aspernatur architecto excepturi delectus soluta? Ipsa, deleniti dolorem hic consequatur
@@ -36,12 +36,12 @@ function About() {
         </div>
         
         
-        <div class="row align-items-center content">
-            <div class="col-md-6 text-center">
-              <div class="row justify-content-center">
-              <div class="col-10 col-lg-8 mb-5 mb-md-0 blurb m-4 m-md-0">
+        <div className="row align-items-center content">
+            <div className="col-md-6 text-center">
+              <div className="row justify-content-center">
+              <div className="col-10 col-lg-8 mb-5 mb-md-0 blurb m-4 m-md-0">
                         <h3>The wondermom story</h3>
-                        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, iste molestiae
+                        <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, iste molestiae
                             beatae, maiores deserunt
                             in voluptatibus
                             aspernatur architecto excepturi delectus soluta? Ipsa, deleniti dolorem hic consequatur
@@ -52,7 +52,7 @@ function About() {
                     </div>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div className="col-md-6">
             <img src="images/health.jpg" alt="" class="img-fluid"></img>
             </div>
             </div>
