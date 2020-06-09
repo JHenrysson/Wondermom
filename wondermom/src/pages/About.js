c8830e898166b7411e17e942350cda88a815016d
Mira Koivula <mira.koivula.1987@gmail.com>
2020-06-09 11:19:25 +0200

    Update About.js
    added a header

--- wondermom/src/pages/About.js
+++ wondermom/src/pages/About.js
@@ -7,46 +7,25 @@ function About() {
       <>
 
 
-    
-<br></br>
-<br></br>
-<br></br>
-<div class="row align-items-center content">
-            <div class="col-md-6 text-center">
-                <div class="row justify-content-center">
-                    <div class="col-10 col-lg-8-py-5 mb-5 mb-md-0">
-                        <h2>The wondermom story</h2>
-                        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, iste molestiae
-                            beatae, maiores deserunt
-                            in voluptatibus
-                            aspernatur architecto excepturi delectus soluta? Ipsa, deleniti dolorem hic consequatur
-                            repellat eveniet quidem
-                            voluptate necessitatibus dolorum delectus minus vitae, ut, veritatis sint ipsum magnam
-                            autem nam ex deserunt debitis
-                            eaque ratione! Nobis, quidem assumenda.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <img src="images/health.jpg" alt="" class="img-fluid"></img>
-            </div>
-        </div>
-                
 
-<br></br>
-<br></br>
-<br></br>
-        <section class="container-fluid px-0">
+
+<div className="jumbotron jumbotron-fluid text-white d-flex justify-content-center align-items-center" id="aboutjumbotron">
+  <div class="container text-center">
+  <h1 class="display-2">About us</h1>
+  <p class="lead">We’re here to keep mamas around the world inspired and motivated to live healthier every day. We’ll be your best friend in your back pocket, coaching you on how to stay fit, eat well, and live more mindfully.</p>
+</div>
+</div>
+
+
+      <section class="container-fluid px-0">
         <div class="row align-items-center content">
-            <div class="col-md-6">
+          <div class="col-md-6 order-2 order-md-1">
             <img src="./images/heart.jpg" alt="" class="img-fluid"></img>
-            </div>
-
-            
-            <div class="col-md-6 text-center">
+            </div>    
+            <div class="col-md-6 text-center order-1 order-md-2">
                 <div class="row justify-content-center">
-                    <div class="col-10 col-lg-8 mb-5 mb-md-0">
-                        <h2>Our mission</h2>
+                    <div class="col-10 col-lg-8 mb-5 mb-md-0 blurb m-4 m-md-0">
+                        <h3>Our mission</h3>
                         <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, iste molestiae
                             beatae, maiores deserunt
                             in voluptatibus
@@ -59,8 +38,30 @@ function About() {
                 </div>
             </div>
         </div>
+        
+        
+        <div class="row align-items-center content">
+            <div class="col-md-6 text-center">
+              <div class="row justify-content-center">
+              <div class="col-10 col-lg-8 mb-5 mb-md-0 blurb m-4 m-md-0">
+                        <h3>The wondermom story</h3>
+                        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, iste molestiae
+                            beatae, maiores deserunt
+                            in voluptatibus
+                            aspernatur architecto excepturi delectus soluta? Ipsa, deleniti dolorem hic consequatur
+                            repellat eveniet quidem
+                            voluptate necessitatibus dolorum delectus minus vitae, ut, veritatis sint ipsum magnam
+                            autem nam ex deserunt debitis
+                            eaque ratione! Nobis, quidem assumenda.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+            <img src="images/health.jpg" alt="" class="img-fluid"></img>
+            </div>
+            </div>
         </section>
-
+                
    
 
      </>
