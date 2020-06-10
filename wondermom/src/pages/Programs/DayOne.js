0a4c5be79e872e456a13b712ac321b67f9ca27a8
Elinor Kero <elinor.kero@cmeducations.se>
2020-06-10 13:36:55 +0200

    added gifs and made a workout grid on day 1

--- wondermom/src/pages/Programs/DayOne.js
+++ wondermom/src/pages/Programs/DayOne.js
@@ -5,67 +5,78 @@ import './DayOne.css';
 const dayOne = props => (
     <div>
         <div id="jumbotronDayOne" className="jumbotron jumbotron-fluid">
-                <div className="container">
+            <div className="container">
 
-                </div>
-
-
-
-        </div>
-        <div className="media">
-            <img style={{width: 25 + '%'}} className="img-thumbnail align-self-start mr-3" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/whm012320lavenderset-010-1586183286.jpg" alt="Generic placeholder image"/>
-                <div className="card-body media-body">
-                    <h5 className="mt-0">Exercise One</h5>
-                    <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras
-                        purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
-                        vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-                    <p>Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque
-                        penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                </div>
-        </div>
-        <div className="media">
-            <img style={{width: 25 + '%'}} className="img-thumbnail align-self-start mr-3" src="https://media1.popsugar-assets.com/files/thumbor/NvEkx3AENtxWoIIX7WCQXvB-FZ0/307x0:5219x4912/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/07/31/044/n/1922729/8c189b805d422c81c9ba55.76538773_/i/3-Minute-Ab-Workout-Runners.jpg" alt="Generic placeholder image"/>
-            <div className="media-body">
-                <h5 className="mt-0">Exercise Two </h5>
-                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras
-                    purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
-                    vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-                <p>Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque
-                    penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
             </div>
         </div>
-        <div className="media">
-        <img style={{width: 25 + '%'}} className="align-self-start mr-3" src="https://hips.hearstapps.com/sev.h-cdn.co/assets/15/50/3200x3200/square-1449857371-sev1211097b.jpg?resize=320:*" alt="Generic placeholder image"/>
-        <div className="media-body card-body">
-            <h5 className="mt-0">Exercise Three</h5>
-            <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras
-                purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
-                vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-            <p>Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque
-                penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-        </div>
-    </div>
-        <div className="media">
-        <img style={{width: 25 + '%'}} className="align-self-start mr-3" src="https://i0.wp.com/thumbs.gfycat.com/UncomfortableElementaryIberianbarbel-size_restricted.gif?w=1155&h=840" alt="Generic placeholder image"/>
-        <div className="media-body">
-            <h5 className="mt-0">Exercise Four </h5>
-            <p>Trying this out... with GIFs // Not sure how the resizing will work Nulla vel metus scelerisque ante sollicitudin. Cras
-                purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
-                vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-            <p>Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque
-                penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-        </div>
-    </div>
-        <div className="media">
-        <img style={{width: 25 + '%'}} className="align-self-start mr-3" src="https://media1.popsugar-assets.com/files/thumbor/MChgD68xTjg7NF73eCEJSPlSG3I/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/11/15/922/n/1922729/20b968b15a0cacb6ecc572.55540901_edit_img_image_44265073_1510776963/i/Lower-Body-Exercises-Women.jpg" alt="Generic placeholder image"/>
-        <div className="media-body">
-            <h5 className="mt-0">Exercise Five</h5>
-            <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras
-                purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
-                vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-            <p>Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque
-                penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-        </div>
+
+        <div className="container">
+            <div className="row">
+                <div className="col-sm grid-item item1">
+                    <img className="img-thumbnail align-self-start mr-3"
+                        src="/wondermom/images/plankstretch.gif"
+                        alt="Generic placeholder image" />
+                </div>
+                <div className="col-sm grid-item item2">
+                    Exercise 1
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-sm grid-item item3">
+                    <img className="img-thumbnail align-self-start mr-3"
+                        src="/wondermom/images/mountainclimbers.gif"
+                        alt="Generic placeholder image" />
+                </div>
+                <div className="col-sm grid-item item4">
+                    Exercise 2
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-sm grid-item item5">
+                    <img className="img-thumbnail align-self-start mr-3"
+                        src="/wondermom/images/planktwist.gif"
+                        alt="Generic placeholder image" />
+                </div>
+                <div className="col-sm grid-item item6">
+                    Exercise 3
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-sm grid-item item7">
+                    <img className="img-thumbnail align-self-start mr-3"
+                        src="/wondermom/images/squats.gif"
+                        alt="Generic placeholder image" />
+                </div>
+                <div className="col-sm grid-item item8">
+                    Exercise 4
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-sm grid-item item9">
+                    <img className="img-thumbnail align-self-start mr-3"
+                        src="/wondermom/images/backlift.gif"
+                        alt="Generic placeholder image" />
+                </div>
+                <div className="col-sm grid-item item10">
+                    Exercise 5
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-sm grid-item item11">
+                    <img className="img-thumbnail align-self-start mr-3"
+                        src="/wondermom/images/pushups.gif"
+                        alt="Generic placeholder image" />
+                </div>
+                <div className="col-sm grid-item item12">
+                    Exercise 5
+                </div>
+            </div>
+
         </div>
 
 
