d8606d7ac78e15e0fadd1698ef64ae205a9973da
carolinearousell <caroline.arousell@cmeducations.se>
2020-06-10 12:13:48 +0200

    did some styling

--- wondermom/src/pages/Login.js
+++ wondermom/src/pages/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./Login.css"
 import { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';   
 import { Form, Button } from 'react-bootstrap';
@@ -8,25 +9,32 @@ class Login extends Component {
   render() {  
 
   return (
-      
+
+
+<div className="wrapper">
+
 <Form>
 <Form.Group controlId="formBasicEmail">
-  <Form.Label>Email address</Form.Label>
-  <Form.Control type="email" placeholder="Enter email" />
-  <Form.Text className="text-muted">
-    We'll never share your email with anyone else.
-  </Form.Text>
+  <Form.Label id="emailText">Email address</Form.Label>
+  <Form.Control type="email" placeholder="Enter email"/>
 </Form.Group>
 
 <Form.Group controlId="formBasicPassword">
-  <Form.Label>Password</Form.Label>
-  <Form.Control type="password" placeholder="Password" />
+  <Form.Label id="passwordText">Password</Form.Label>
+  <Form.Control type="password" placeholder="Password"/>
 </Form.Group>
-<Button variant="primary" type="submit">
-  Submit
+<div className="text-center">
+<Button variant="primary btn-block" type="submit" id="loginButton">
+  Log In
 </Button>
+<Button variant="primary btn-block" type="submit" id="signinButton">
+  Sign In
+</Button>
+</div>
+
 </Form>
 
+</div> 
   );
   }
 };
