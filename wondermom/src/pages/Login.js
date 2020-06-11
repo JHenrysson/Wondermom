71411cce55ac68a89fa0dcda277bc1bbc04062cd
carolinearousell <caroline.arousell@cmeducations.se>
2020-06-11 10:32:37 +0200

    tried making password and things work

--- wondermom/src/pages/Login.js
+++ wondermom/src/pages/Login.js
@@ -6,6 +6,54 @@ import { Form, Button } from 'react-bootstrap';
 
 class Login extends Component {  
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        validation: {
+            email: {
+                status: 'pristine',
+                error: ''
+            },
+            password: {
+                status: 'pristine',
+                error: ''
+            },
+            form: {
+                disabled: true,
+                error: ''
+            }
+        }
+    }
+}
+
+/*signupUser = () => {
+  window.auth.createUserWithEmailAndPassword(
+    this.email, 
+    this.password
+  );
+  loginUser = () => {
+    window.auth.signInWithEmailAndPassword(
+      this.email,
+      this.password
+    );
+  }
+}
+*/
+
+validatePassword = (e) => {
+  const value = e.target.value;
+  const validation = { ...this.state.validation };
+  const newStatus = value.length > 5 ? 'is-valid' : 'is-invalid';
+  if (newStatus != validation.password.status) {
+      validation.password.status = newStatus;
+      validation.password.error = newStatus == 'is-invalid' ? 'Must be at least 6 characters!' : '';
+      this.setState({ validation });
+  }
+  this.password = value;
+  this.validateForm();
+}
+
   render() {  
 
   return (
@@ -17,12 +65,12 @@ class Login extends Component {
 <Form>
 <Form.Group controlId="formBasicEmail">
   <Form.Label id="emailText">Email address</Form.Label>
-  <Form.Control type="email" placeholder="Enter email"/>
+  <Form.Control type="email" placeholder="Enter email" label="Email" name="email" className={this.state.validation.email.status} onChange={this.validateEmail}/>
 </Form.Group>
 
 <Form.Group controlId="formBasicPassword">
   <Form.Label id="passwordText">Password</Form.Label>
-  <Form.Control type="password" placeholder="Password"/>
+  <Form.Control type="password" placeholder="Password" className={this.state.validation.password.status} label="Password"  name="password"  onChange={this.validatePassword}/>
 </Form.Group>
 
 <div className="text-center">
