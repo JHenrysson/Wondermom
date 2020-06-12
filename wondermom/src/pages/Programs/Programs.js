f9ccb015a5cd6680e6bfa65d716a1cbca8d9e4a1
Elinor Kero <elinor.kero@cmeducations.se>
2020-06-12 13:28:21 +0200

    changed button position and replaced images

--- wondermom/src/pages/Programs/Programs.js
+++ wondermom/src/pages/Programs/Programs.js
@@ -21,30 +21,28 @@ class Programs extends Component {
                 <div id="main">
                     <div className="card-deck">
                         <div className="card">
-                            <img src="./images/6weeks.jpg" className="card-img-top" alt="6weeks"></img>
+                            <img src="./images/6weeks2.jpg" className="program-img card-img-top" alt="6weeks"></img>
+                            <a id="tryThisButton" href="SixWeekProgram" className="centerButton card-link justify-content-center">6 Weeks</a>
                             <div className="card-body">
-                                <h4 className="card-title">6 Week Program</h4>
                                 <p className="card-text">This is where the user will find information about the workouts plans offered by Wondermom. Here there will be a summary explaining the workout plan and its areas of focus and time needed to complete.
                                     The user can click the button to see more about the workout plan they choose. </p>
-                                <a id="tryThisButton" href="SixWeekProgram" className="centerButton card-link justify-content-center">Try This Program</a>
+                                
                             </div>
                         </div>
                         <div className="card">
                             <img src="./images/8weeks.jpg" className="card-img-top" alt="8weeks"></img>
+                            <a id="tryThisButton" href="/SixWeekProgram" className="centerButton card-link justify-content-center">8 Weeks</a>
                             <div className="card-body">
-                                <h4 className="card-title">8 Week Program</h4>
                                 <p className="card-text">This is where the user will find information about the workouts plans offered by Wondermom. Here there will be a summary explaining the workout plan and its areas of focus and time needed to complete.
                                     The user can click the button to see more about the workout plan they choose. </p>
-                                <a id="tryThisButton" href="/SixWeekProgram" className="centerButton card-link justify-content-center">Try This Program</a>
                             </div>
                         </div>
                         <div className="card">
                             <img src="./images/12weeks.jpg" className="card-img-top" alt="12weeks"></img>
+                            <a id="tryThisButton" href="/SixWeekProgram" className="centerButton card-link justify-content-center">12 Weeks</a>
                             <div className="card-body">
-                                <h4 className="card-title">12 Week Program</h4>
                                 <p className="card-text">This is where the user will find information about the workouts plans offered by Wondermom. Here there will be a summary explaining the workout plan and its areas of focus and time needed to complete.
                                     The user can click the button to see more about the workout plan they choose. </p>
-                                <a id="tryThisButton" href="/SixWeekProgram" className="centerButton card-link justify-content-center">Try This Program</a>
                             </div>
                         </div>
                     </div>
