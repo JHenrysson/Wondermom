338241b13046446aaa04264fbde4dd773a99c503
Elinor Kero <elinor.kero@cmeducations.se>
2020-06-16 17:45:18 +0200

    Edited sign up button on login page and added Log in header

--- wondermom/src/pages/Login.js
+++ wondermom/src/pages/Login.js
@@ -4,120 +4,124 @@ import { Component } from 'react'
 import { Form, Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
-class Login extends Component {  
+class Login extends Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-        validation: {
-            email: {
-                status: 'pristine',
-                error: ''
-            },
-            password: {
-                status: 'pristine',
-                error: ''
-            },
-            form: {
-                disabled: true,
-                error: ''
-            }
+      validation: {
+        email: {
+          status: 'pristine',
+          error: ''
+        },
+        password: {
+          status: 'pristine',
+          error: ''
+        },
+        form: {
+          disabled: true,
+          error: ''
         }
+      }
     }
-}
+  }
 
   signup = () => {
     window.auth.createUserWithEmailAndPassword(
-      this.email, 
+      this.email,
       this.password
-  ).then(() => {
-    window.location.href = '/home';
-  }).catch(error => {
-    
-  });
+    ).then(() => {
+      window.location.href = '/home';
+    }).catch(error => {
+
+    });
   }
 
 
-    
+
   login = () => {
     window.auth.signInWithEmailAndPassword(
       this.email,
       this.password
-      ).then(() => {
-        window.location.href = '/home';
-      }).catch(error => {
-        
-      });
-      }
+    ).then(() => {
+      window.location.href = '/home';
+    }).catch(error => {
+
+    });
+  }
 
 
-validateEmail = (e) => {
-  const value = e.target.value;
-  const validation = { ...this.state.validation };
-  const newStatus = /^[^@]+@[^@]+$/.test(value) ? 'is-valid' : 'is-invalid';
-  if (newStatus != validation.email.status) {
+  validateEmail = (e) => {
+    const value = e.target.value;
+    const validation = { ...this.state.validation };
+    const newStatus = /^[^@]+@[^@]+$/.test(value) ? 'is-valid' : 'is-invalid';
+    if (newStatus != validation.email.status) {
       validation.email.status = newStatus;
       validation.email.error = newStatus == 'is-invalid' ? 'Not a valid email address!' : '';
       this.setState({ validation });
+    }
+    this.email = value;
+    this.validateForm();
   }
-  this.email = value;
-  this.validateForm();
-}
 
 
-validatePassword = (e) => {
-  const value = e.target.value;
-  const validation = { ...this.state.validation };
-  const newStatus = value.length > 5 ? 'is-valid' : 'is-invalid';
-  if (newStatus != validation.password.status) {
+  validatePassword = (e) => {
+    const value = e.target.value;
+    const validation = { ...this.state.validation };
+    const newStatus = value.length > 5 ? 'is-valid' : 'is-invalid';
+    if (newStatus != validation.password.status) {
       validation.password.status = newStatus;
       validation.password.error = newStatus == 'is-invalid' ? 'Must be at least 6 characters!' : '';
       this.setState({ validation });
+    }
+    this.password = value;
+    this.validateForm();
   }
-  this.password = value;
-  this.validateForm();
-}
 
-validateForm() {
-  const validation = { ...this.state.validation };
-  const isFormDisabled = validation.email.status != 'is-valid' || validation.password.status != 'is-valid'; 
-  if (isFormDisabled != validation.form.disabled) {
+  validateForm() {
+    const validation = { ...this.state.validation };
+    const isFormDisabled = validation.email.status != 'is-valid' || validation.password.status != 'is-valid';
+    if (isFormDisabled != validation.form.disabled) {
       validation.form.disabled = isFormDisabled;
       this.setState({ validation });
+    }
   }
-}
 
-  render() {  
+  render() {
 
-  return (
-    <div className="wrapper">
-      <div className="form-container">
-        <Form>
-          <Form.Group controlId="formBasicEmail">
+    return (
+      <div className="wrapper">
+        <div className="form-container">
+          <Form>
+            <div className="text-center">
+              <h3 id="signUp">Log in</h3>
+            </div>
+
+            <Form.Group controlId="formBasicEmail">
               <Form.Label id="emailText">Email address</Form.Label>
               <Form.Control type="email" placeholder="Enter email" label="Email" name="email" className={this.state.validation.email.status} onChange={this.validateEmail} />
-          </Form.Group>
+            </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
+            <Form.Group controlId="formBasicPassword">
               <Form.Label id="passwordText">Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" className={this.state.validation.password.status} label="Password"  name="password"  onChange={this.validatePassword}/>
-            <Form.Text className="textpassword">
-              Password needs to be at least 6 characters.
+              <Form.Control type="password" placeholder="Password" className={this.state.validation.password.status} label="Password" name="password" onChange={this.validatePassword} />
+              <Form.Text className="textpassword">
+                Password needs to be at least 6 characters.
             </Form.Text>
-          </Form.Group>
+            </Form.Group>
 
-          <div className="text-center">
+            <div className="text-center">
               <Button variant="primary btn-block" type="button" id="loginButton" disabled={this.state.validation.form.disabled} onClick={this.login}>
                 Log In
               </Button>
 
-              <Link to="signup" className= "btn btn-block" id="signupButton">Sign Up</Link>
-          </div>
-        </Form>
+              <Link to="signup" className="btn" id="signUpButton">Not a member yet? Sign up here!</Link>
+            </div>
+          </Form>
+        </div>
       </div>
-    </div> 
-  );
+    );
   }
 };
 
