d5d800b2b73cc4cd8bba50b7e79ddaa7056e43ba
Mira Koivula <mira.koivula.1987@gmail.com>
2020-06-18 12:09:57 +0200

    Update About.js

--- wondermom/src/pages/About.js
+++ wondermom/src/pages/About.js
@@ -7,25 +7,66 @@ function About() {
       <>
 
 <div id="jumbotronabout" className="jumbotron jumbotron-fluid px-0">
-                    <div className="container-fluid px-0"></div>
+                    <div className="container-fluid px-0">
+                    </div>
                 </div>
 
                 <div className="info-panel text-center">
     <div class="container p-5">
         <div className="container">
             <div className="row justify-content-center">
-            <div className="col-md-8">
-      <h2 className=" pb-3 text-dark">Created to help moms to live a better, stronger and healthier life.</h2>
-      <p class="nopad">Lorem ipsus dolor lorem ipsis dlolo Lorem ipsus dolor lorem ipsis dlolo Lorem ipsus dolor lorem ipsis 
-            dlolo Lorem ipsus dolor lorem ipsis dlolo. Lorem ipsus dolor lorem ipsis dlolo Lorem ipsus dolor lorem ipsis dlolo Lorem ipsus dolor lorem ipsis 
-            dlolo Lorem ipsus dolor lorem ipsis dlolo.</p>
+            <div className="col-md-10">
+                <div className="info-header py-5">
+      <h2 className="pb-3 text-dark">Created to help moms to live a better, stronger and healthier life.</h2>
+      
+            </div>
+         </div>
+         </div>
     </div>
  </div>
  </div>
- </div>
- </div>
+ 
+ 
+ <section className="container">
+      <div className="row align-items-center content">
+          <div className="col-md-6 order-2 order-md-1">
+            <img src="./images/health.jpg" alt="" class="img-fluid"></img>
+            </div>    
+            <div className="col-md-6 text-center order-1 order-md-2">
+                <div className="row justify-content-center">
+                    <div className="col-10 col-lg-8 mb-5 blurb m-md-0">
+                        <h3>THE WONDERMOM STORY</h3>
+                        <p className="lead">Lorem ipsus dolor lorem ipsis dlolo Lorem ipsus dolor lorem ipsis dlolo Lorem ipsus dolor lorem ipsis 
+                    dlolo Lorem ipsus dolor lorem ipsis dlolo. Lorem ipsus dolor lorem ipsis dlolo Lorem ipsus dolor lorem ipsis dlolo Lorem ipsus dolor lorem ipsis 
+            dlolo Lorem ipsus dolor lorem ipsis dlolo</p>
 
-                <div className="fluidbanner">
+                    </div>
+                </div>
+            </div>
+            </div>
+            </section>
+
+<section className="container mt-5">
+            <div className="row align-items-center content">
+            <div className="col-md-6 text-center">
+              <div className="row justify-content-center">
+              <div className="col-10 col-lg-8 mb-5 mb-md-0 blurb m-4 m-md-0">
+                        <h3>OUR MISSION</h3>
+                        <p className="lead">Lorem ipsus dolor lorem ipsis dlolo Lorem ipsus dolor lorem ipsis dlolo Lorem ipsus dolor lorem ipsis 
+                    dlolo Lorem ipsus dolor lorem ipsis dlolo. Lorem ipsus dolor lorem ipsis dlolo Lorem ipsus dolor lorem ipsis dlolo Lorem ipsus dolor lorem ipsis 
+            dlolo Lorem ipsus dolor lorem ipsis dlolo </p>
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-6">
+            <img src="images/phone.jpg" alt="" class="img-fluid"></img>
+            </div>
+            </div>
+        </section>
+ 
+
+{/*
+    <div className="fluidbanner">
     <div className="container py-5">
       <div className="container">
       <div className="row">
@@ -47,16 +88,19 @@ function About() {
     </div>
   </div>
 
+
+  
+
       <div className="fluidbanner2">
-        <div className="container py-5">
+        <div className="container py-3">
           <div className="container">
             <div className="row">
               <div className="col-md-6 order-2 order-md-1">
         <img src="./images/health.jpg" alt="" class="img-fluid"></img>
         </div>
   
-        <div className="col-md-6 order-1 order-md-2 px-5 align-self-center">
-        <div className="textruta m-md-0">
+        <div className="col-md-6 order-1 order-md-2 px-5 align-self-center mb-md-0">
+        <div className="textruta">
           <h3 class="text-dark font-weight-bold">OUR MISSION</h3>
           <p>Lorem ipsus dolor lorem ipsis dlolo Lorem ipsus dolor lorem ipsis dlolo Lorem ipsus dolor lorem ipsis 
             dlolo Lorem ipsus dolor lorem ipsis dlolo
