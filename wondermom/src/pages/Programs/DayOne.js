74bade2d1576449b7c57940c6424e6f830b963a7
Elinor Kero <elinor.kero@cmeducations.se>
2020-06-18 16:08:08 +0200

    made checkbox button react style

--- wondermom/src/pages/Programs/DayOne.js
+++ wondermom/src/pages/Programs/DayOne.js
@@ -7,42 +7,48 @@ const exercises = [
         title: 'Exercise 1',
         img: '/wondermom/images/plankstretch.gif',
         description: 'Description on how to best perform the exercise.',
-        reps: 'Do 3 sets with 10 reps.'
+        reps: 'Do 3 sets with 10 reps.',
+        check: 'Check when done'
     },
     {
         slug: 'Exercise 2',
         title: 'Exercise 2',
         img: '/wondermom/images/mountainclimbers.gif',
         description: 'Description on how to best perform the exercise.',
-        reps: 'Do 3 sets with 10 reps.'
+        reps: 'Do 3 sets with 10 reps.',
+        check: 'Check when done'
     },
     {
         slug: 'Exercise 3',
         title: 'Exercise 3',
         img: '/wondermom/images/planktwist.gif',
         description: 'Description on how to best perform the exercise.',
-        reps: 'Do 3 sets with 10 reps.'
+        reps: 'Do 3 sets with 10 reps.',
+        check: 'Check when done'
     },
     {
         slug: 'Exercise 4',
         title: 'Exercise 4',
         img: '/wondermom/images/squats.gif',
         description: 'Description on how to best perform the exercise.',
-        reps: 'Do 3 sets with 10 reps.'
+        reps: 'Do 3 sets with 10 reps.',
+        check: 'Check when done'
     },
     {
         slug: 'Exercise 5',
         title: 'Exercise 5',
         img: '/wondermom/images/backlift.gif',
         description: 'Description on how to best perform the exercise.',
-        reps: 'Do 3 sets with 10 reps.'
+        reps: 'Do 3 sets with 10 reps.',
+        check: 'Check when done'
     },
     {
         slug: 'Exercise 6',
         title: 'Exercise 6',
         img: '/wondermom/images/pushups.gif',
         description: 'Description on how to best perform the exercise.',
-        reps: 'Do 3 sets with 10 reps.'
+        reps: 'Do 3 sets with 10 reps.',
+        check: 'Check when done'
     }
 ];
 
@@ -72,7 +78,7 @@ const dayOne = props => (
                                     <p id="descriptionText">{exercises.reps} </p>
                                     <br />
                                     <label className="checkboxContainer">
-                                        <input type="checkbox" /> Check when done
+                                        <input type="checkbox" /> {exercises.check}
                                         <span className="checkmark" />
                                     </label>
                                 </div>
