a89b828bff0ffb3709077b5e6e2f7ba088a8404f
Elinor Kero <elinor.kero@cmeducations.se>
2020-06-18 15:56:39 +0200

    changed styling on checkboxes

--- wondermom/src/pages/Programs/DayOne.js
+++ wondermom/src/pages/Programs/DayOne.js
@@ -7,36 +7,42 @@ const exercises = [
         title: 'Exercise 1',
         img: '/wondermom/images/plankstretch.gif',
         description: 'Description on how to best perform the exercise.',
+        reps: 'Do 3 sets with 10 reps.'
     },
     {
         slug: 'Exercise 2',
         title: 'Exercise 2',
         img: '/wondermom/images/mountainclimbers.gif',
         description: 'Description on how to best perform the exercise.',
+        reps: 'Do 3 sets with 10 reps.'
     },
     {
         slug: 'Exercise 3',
         title: 'Exercise 3',
         img: '/wondermom/images/planktwist.gif',
         description: 'Description on how to best perform the exercise.',
+        reps: 'Do 3 sets with 10 reps.'
     },
     {
         slug: 'Exercise 4',
         title: 'Exercise 4',
         img: '/wondermom/images/squats.gif',
         description: 'Description on how to best perform the exercise.',
+        reps: 'Do 3 sets with 10 reps.'
     },
     {
         slug: 'Exercise 5',
         title: 'Exercise 5',
         img: '/wondermom/images/backlift.gif',
         description: 'Description on how to best perform the exercise.',
+        reps: 'Do 3 sets with 10 reps.'
     },
     {
         slug: 'Exercise 6',
         title: 'Exercise 6',
         img: '/wondermom/images/pushups.gif',
         description: 'Description on how to best perform the exercise.',
+        reps: 'Do 3 sets with 10 reps.'
     }
 ];
 
@@ -63,8 +69,10 @@ const dayOne = props => (
                                 <div className="col-sm grid-item item2">
                                     <h3 id="headerOne" className="card-title">{exercises.title}</h3>
                                     <p id="descriptionText">{exercises.description} </p>
+                                    <p id="descriptionText">{exercises.reps} </p>
+                                    <br />
                                     <label className="checkboxContainer">
-                                        <input type="checkbox" /> <p>Check when done</p>
+                                        <input type="checkbox" /> Check when done
                                         <span className="checkmark" />
                                     </label>
                                 </div>
